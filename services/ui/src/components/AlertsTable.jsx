0df65f68f545ebaff9b1c1af1810b6677d1e686e
Chundyy <goncaloleitao27@gmail.com>
2026-01-22 17:06:45 +0000

    Ajuste do docker e elastic

--- services/ui/src/components/AlertsTable.jsx
+++ services/ui/src/components/AlertsTable.jsx
@@ -2,9 +2,9 @@ export default function AlertsTable() {
   const alerts = [
     { time: "10:21", severity: "CRITICAL", src: "192.168.1.10", desc: "SQL Injection Attempt" },
     { time: "10:10", severity: "HIGH", src: "192.168.1.22", desc: "Port Scan Detected" },
-    { time: "09:50", severity: "MEDIUM", src: "192.168.1.33", desc: "Suspicious DNS Query" }
+    { time: "09:50", severity: "MEDIUM", src: "192.168.1.33", desc: "Suspicious DNS Query" },
     { time: "11:50", severity: "LOW", src: "192.168.3.33", desc: "SUspicious IP" }
-  ]
+  ];
 
   return (
     <table className="alert-table">
